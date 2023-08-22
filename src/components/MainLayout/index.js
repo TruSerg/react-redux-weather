59fbf6b531f700a8dee956d0181670af8fce498e
Sergey <truhan_1984@mail.ru>
2023-08-22 18:10:46 +0300

    added all weather bg

--- src/components/MainLayout/index.js
+++ src/components/MainLayout/index.js
@@ -1,3 +1,4 @@
+import { useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 
 import Header from "../Header";
@@ -10,44 +11,48 @@ import bgRain from "../../static/img/bg-rain.jpg";
 import bgThunderstorm from "../../static/img/bg-thunderstorm.jpg";
 import bgFog from "../../static/img/bg-fog.jpg";
 import bgSnow from "../../static/img/bg-snow.jpg";
+import bgDust from "../../static/img/bg-dust.jpg";
+import bgAsh from "../../static/img/bg-ash.jpg";
+import bgSquall from "../../static/img/bg-squall.jpg";
+import bgTornado from "../../static/img/bg-tornado.jpg";
 
 import style from "./styles.module.scss";
 
 const MainLayout = ({ children }) => {
+  const [weatherBG, setWeatherBG] = useState(bg);
+
   const { currentWeather } = useSelector((state) => state.weatherData);
+
+  const bgOfCurrentWeather = {
+    Clear: bgSunny,
+    Rain: bgRain,
+    Thunderstorm: bgThunderstorm,
+    Clouds: bgClouds,
+    Fog: bgFog,
+    Mist: bgFog,
+    Smoke: bgFog,
+    Snow: bgSnow,
+    Haze: bgFog,
+    Drizzle: bgRain,
+    Dust: bgDust,
+    Sand: bgDust,
+    Ash: bgAsh,
+    Squall: bgSquall,
+    Tornado: bgTornado,
+  };
+
+  useMemo(() => {
+    for (const weather in bgOfCurrentWeather) {
+      if (weather === currentWeather) {
+        setWeatherBG(bgOfCurrentWeather[weather]);
+      }
+    }
+  }, [currentWeather]);
 
   return (
     <div
       className={style.mainWrapper}
-      style={
-        (currentWeather === "Clear" && {
-          backgroundImage: `url(${bgSunny})`,
-        }) ||
-        (currentWeather === "Rain" && {
-          backgroundImage: `url(${bgRain})`,
-        }) ||
-        (currentWeather === "Thunderstorm" && {
-          backgroundImage: `url(${bgThunderstorm})`,
-        }) ||
-        (currentWeather === "Clouds" && {
-          backgroundImage: `url(${bgClouds})`,
-        }) ||
-        (currentWeather === "Fog" && {
-          backgroundImage: `url(${bgFog})`,
-        }) ||
-        (currentWeather === "Mist" && {
-          backgroundImage: `url(${bgFog})`,
-        }) ||
-        (currentWeather === "Smoke" && {
-          backgroundImage: `url(${bgFog})`,
-        }) ||
-        (currentWeather === "Snow" && {
-          backgroundImage: `url(${bgSnow})`,
-        }) ||
-        (currentWeather === "Haze" && {
-          backgroundImage: `url(${bgFog})`,
-        }) || { backgroundImage: `url(${bg})` }
-      }
+      style={{ backgroundImage: `url(${weatherBG})` }}
     >
       <Header />
 
