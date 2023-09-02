243a262a3930296b76064b9e47d1dc0e8e670823
Sergey <truhan_1984@mail.ru>
2023-09-02 17:52:55 +0300

    fixed show state bg

--- src/components/MainLayout/index.js
+++ src/components/MainLayout/index.js
@@ -1,66 +1,66 @@
-import { useMemo, useState } from "react";
-import { useSelector } from "react-redux";
+import { useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
 
-import Header from "../Header";
-import Footer from "../Footer";
+import Header from '../Header';
+import Footer from '../Footer';
 
-import bg from "../../static/img/bg.jpg";
-import bgSunny from "../../static/img/bg-sunny.jpg";
-import bgClouds from "../../static/img/bg-clouds.jpg";
-import bgRain from "../../static/img/bg-rain.jpg";
-import bgThunderstorm from "../../static/img/bg-thunderstorm.jpg";
-import bgFog from "../../static/img/bg-fog.jpg";
-import bgSnow from "../../static/img/bg-snow.jpg";
-import bgDust from "../../static/img/bg-dust.jpg";
-import bgAsh from "../../static/img/bg-ash.jpg";
-import bgSquall from "../../static/img/bg-squall.jpg";
-import bgTornado from "../../static/img/bg-tornado.jpg";
+import bg from '../../static/img/bg.jpg';
+import bgSunny from '../../static/img/bg-sunny.jpg';
+import bgClouds from '../../static/img/bg-clouds.jpg';
+import bgRain from '../../static/img/bg-rain.jpg';
+import bgThunderstorm from '../../static/img/bg-thunderstorm.jpg';
+import bgFog from '../../static/img/bg-fog.jpg';
+import bgSnow from '../../static/img/bg-snow.jpg';
+import bgDust from '../../static/img/bg-dust.jpg';
+import bgAsh from '../../static/img/bg-ash.jpg';
+import bgSquall from '../../static/img/bg-squall.jpg';
+import bgTornado from '../../static/img/bg-tornado.jpg';
 
-import style from "./styles.module.scss";
+import style from './styles.module.scss';
 
 const MainLayout = ({ children }) => {
-  const [weatherBG, setWeatherBG] = useState(bg);
+	const [weatherBG, setWeatherBG] = useState(bg);
 
-  const { currentWeather } = useSelector((state) => state.weatherData);
+	const { currentWeather } = useSelector(state => state.weatherData);
 
-  const bgOfCurrentWeather = {
-    Clear: bgSunny,
-    Rain: bgRain,
-    Thunderstorm: bgThunderstorm,
-    Clouds: bgClouds,
-    Fog: bgFog,
-    Mist: bgFog,
-    Smoke: bgFog,
-    Snow: bgSnow,
-    Haze: bgFog,
-    Drizzle: bgRain,
-    Dust: bgDust,
-    Sand: bgDust,
-    Ash: bgAsh,
-    Squall: bgSquall,
-    Tornado: bgTornado,
-  };
+	const bgOfCurrentWeather = {
+		Clear: bgSunny,
+		Rain: bgRain,
+		Thunderstorm: bgThunderstorm,
+		Clouds: bgClouds,
+		Fog: bgFog,
+		Mist: bgFog,
+		Smoke: bgFog,
+		Snow: bgSnow,
+		Haze: bgFog,
+		Drizzle: bgRain,
+		Dust: bgDust,
+		Sand: bgDust,
+		Ash: bgAsh,
+		Squall: bgSquall,
+		Tornado: bgTornado,
+	};
 
-  useMemo(() => {
-    for (const weather in bgOfCurrentWeather) {
-      if (weather === currentWeather) {
-        setWeatherBG(bgOfCurrentWeather[weather]);
-      }
-    }
-  }, [currentWeather]);
+	useMemo(() => {
+		for (const weather in bgOfCurrentWeather) {
+			if (weather === currentWeather) {
+				setWeatherBG(bgOfCurrentWeather[weather]);
+			}
+		}
+	}, [currentWeather]);
 
-  return (
-    <div
-      className={style.mainWrapper}
-      style={{ backgroundImage: `url(${weatherBG})` }}
-    >
-      <Header />
+	return (
+		<div
+			className={style.mainWrapper}
+			style={{ backgroundImage: `url(${weatherBG})` }}
+		>
+			<Header />
 
-      {children}
+			{children}
 
-      <Footer />
-    </div>
-  );
+			<Footer />
+		</div>
+	);
 };
 
 export default MainLayout;
--- src/store/weatherDataSlice.js
+++ src/store/weatherDataSlice.js
@@ -1,67 +1,67 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-import api from "../http";
+import api from '../http';
 
 export const fetchWeatherData = createAsyncThunk(
-  "weatherData/fetchWeatherData",
-  async (cityName, { rejectedWithValue }) => {
-    try {
-      const res = await api.get(`weather?q=${cityName}`);
+	'weatherData/fetchWeatherData',
+	async (cityName, { rejectedWithValue }) => {
+		try {
+			const res = await api.get(`weather?q=${cityName}`);
 
-      const data = res.data;
+			const data = res.data;
 
-      return data;
-    } catch (error) {
-      return rejectedWithValue(error.message);
-    }
-  }
+			return data;
+		} catch (error) {
+			return rejectedWithValue(error.message);
+		}
+	}
 );
 
 const weatherDataSlice = createSlice({
-  name: "weatherData",
-  initialState: {
-    weatherData: {},
-    currentWeather: "",
-    cityName: "",
-    lon: "",
-    lat: "",
-    temp: "",
-    tempMin: "",
-    tempMax: "",
-    feelsLike: "",
-    windSpeed: "",
-    windDirection: "",
-    pressure: "",
-    humidity: "",
-    weather: [],
-    isLoading: false,
-  },
+	name: 'weatherData',
+	initialState: {
+		weatherData: {},
+		currentWeather: '',
+		cityName: '',
+		lon: '',
+		lat: '',
+		temp: '',
+		tempMin: '',
+		tempMax: '',
+		feelsLike: '',
+		windSpeed: '',
+		windDirection: '',
+		pressure: '',
+		humidity: '',
+		weather: [],
+		isLoading: false,
+	},
 
-  extraReducers: {
-    [fetchWeatherData.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [fetchWeatherData.fulfilled]: (state, { payload }) => {
-      state.weatherData = payload;
-      state.cityName = payload.name;
-      state.currentWeather = payload.weather.map(({ main }) => main).join();
-      state.lon = payload.coord.lon;
-      state.lat = payload.coord.lat;
-      state.temp = payload.main.temp;
-      state.tempMin = payload.main.temp_min;
-      state.tempMax = payload.main.temp_max;
-      state.feelsLike = payload.main.feels_like;
-      state.windSpeed = payload.wind.speed;
-      state.windDirection = payload.wind.deg;
-      state.pressure = payload.main.pressure;
-      state.humidity = payload.main.humidity;
-      state.weather = payload.weather;
-      state.isLoading = false;
-    },
-    [fetchWeatherData.rejected]: (state) => {
-      state.isLoading = false;
-    },
-  },
+	extraReducers: {
+		[fetchWeatherData.pending]: state => {
+			state.isLoading = true;
+		},
+		[fetchWeatherData.fulfilled]: (state, { payload }) => {
+			state.weatherData = payload;
+			state.cityName = payload.name;
+			state.currentWeather = payload.weather[0].main;
+			state.lon = payload.coord.lon;
+			state.lat = payload.coord.lat;
+			state.temp = payload.main.temp;
+			state.tempMin = payload.main.temp_min;
+			state.tempMax = payload.main.temp_max;
+			state.feelsLike = payload.main.feels_like;
+			state.windSpeed = payload.wind.speed;
+			state.windDirection = payload.wind.deg;
+			state.pressure = payload.main.pressure;
+			state.humidity = payload.main.humidity;
+			state.weather = payload.weather;
+			state.isLoading = false;
+		},
+		[fetchWeatherData.rejected]: state => {
+			state.isLoading = false;
+		},
+	},
 });
 
 export default weatherDataSlice.reducer;
