8f4ee3ab03523cc70c92a72e84a43bcd12f51158
Sergey <truhan_1984@mail.ru>
2023-09-10 17:13:43 +0300

    fixed weather-daily layout

--- src/components/WindNavigation/index.js
+++ src/components/WindNavigation/index.js
@@ -1,11 +1,13 @@
-import { NavigationSharp } from "@mui/icons-material";
+import { NavigationSharp } from '@mui/icons-material';
+
+import style from './styles.module.scss';
 
 const WindNavigation = ({ deg }) => {
-  return (
-    <p style={{ transform: `rotate(${deg + 180}deg)` }}>
-      <NavigationSharp fontSize="small" />
-    </p>
-  );
+	return (
+		<p style={{ transform: `rotate(${deg + 180}deg)` }}>
+			<NavigationSharp className={style.windArrow} fontSize='inherit' />
+		</p>
+	);
 };
 
 export default WindNavigation;
