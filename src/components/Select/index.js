a556e867cb1dfaf36939f6880ca7d2499771306c
Sergey <truhan_1984@mail.ru>
2023-09-10 18:53:54 +0300

    fixed select and input layout

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,32 +1,32 @@
-import { memo } from "react";
-import { FormControl, MenuItem, Select, InputLabel } from "@mui/material";
+import { memo } from 'react';
+import { FormControl, MenuItem, Select, InputLabel } from '@mui/material';
 
-import style from "./styles.module.scss";
+import style from './styles.module.scss';
 
 const BasicSelect = ({ value, label, onChange, array }) => {
-  return (
-    <FormControl fullWidth variant="filled">
-      <InputLabel className={style.label} id="demo-simple-select-label">
-        Выберите город...
-      </InputLabel>
-      <Select
-        className={style.select}
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
-        label={label}
-        value={value}
-        onChange={onChange}
-      >
-        {array.map(({ id, name }) => {
-          return (
-            <MenuItem key={id} value={name}>
-              {name}
-            </MenuItem>
-          );
-        })}
-      </Select>
-    </FormControl>
-  );
+	return (
+		<FormControl fullWidth variant='filled'>
+			<InputLabel className={style.label} id='demo-simple-select-label'>
+				Выберите город...
+			</InputLabel>
+			<Select
+				className={style.select}
+				labelId='demo-simple-select-label'
+				id='demo-simple-select'
+				label={label}
+				value={value}
+				onChange={onChange}
+			>
+				{array.map(({ id, name }) => {
+					return (
+						<MenuItem key={id} value={name}>
+							{name}
+						</MenuItem>
+					);
+				})}
+			</Select>
+		</FormControl>
+	);
 };
 
 export default memo(BasicSelect);
--- src/pages/MainWeatherPage/components/MainWeatherPageLayout/index.js
+++ src/pages/MainWeatherPage/components/MainWeatherPageLayout/index.js
@@ -1,90 +1,90 @@
-import Container from "../../../../components/Container";
-import WeatherData from "../../../../components/WeatherData";
-import Loader from "../../../../components/Loader";
-import Select from "../../../../components/Select";
-import DetailsWeatherData from "../../../../components/DetailsWeatherData";
-import WeatherSlider from "../../../../components/WeatherSlider";
-import ShowComponentButton from "../../../../components/Buttons/ShowComponentButton";
+import Container from '../../../../components/Container';
+import WeatherData from '../../../../components/WeatherData';
+import Loader from '../../../../components/Loader';
+import Select from '../../../../components/Select';
+import DetailsWeatherData from '../../../../components/DetailsWeatherData';
+import WeatherSlider from '../../../../components/WeatherSlider';
+import ShowComponentButton from '../../../../components/Buttons/ShowComponentButton';
 
-import style from "./styles.module.scss";
+import style from './styles.module.scss';
 
 const MainWeatherPageLayout = ({
-  isLoading,
-  showMode,
-  weatherDailyList,
-  fiveDaysWeatherList,
-  selectCityNameValue,
-  cityName,
-  temp,
-  tempMin,
-  tempMax,
-  feelsLike,
-  windSpeed,
-  windDirection,
-  pressure,
-  humidity,
-  weather,
-  cities,
-  handleSelectCityNameChange,
-  handleShowModeComponent,
+	isLoading,
+	showMode,
+	weatherDailyList,
+	fiveDaysWeatherList,
+	selectCityNameValue,
+	cityName,
+	temp,
+	tempMin,
+	tempMax,
+	feelsLike,
+	windSpeed,
+	windDirection,
+	pressure,
+	humidity,
+	weather,
+	cities,
+	handleSelectCityNameChange,
+	handleShowModeComponent,
 }) => {
-  return (
-    <div className={style.weather}>
-      <Container>
-        <div className={style.weatherWrapper}>
-          <div className={style.weatherSelectArea}>
-            <div className={style.weatherSelect}>
-              <Select
-                value={selectCityNameValue}
-                label={cityName}
-                array={cities}
-                onChange={handleSelectCityNameChange}
-              />
-            </div>
-          </div>
+	return (
+		<div className={style.weather}>
+			<Container>
+				<div className={style.weatherWrapper}>
+					<div className={style.weatherSelectArea}>
+						<div className={style.weatherSelect}>
+							<Select
+								value={selectCityNameValue}
+								label={cityName}
+								array={cities}
+								onChange={handleSelectCityNameChange}
+							/>
+						</div>
+					</div>
 
-          <>
-            {isLoading ? (
-              <Loader />
-            ) : (
-              <>
-                <WeatherData
-                  temp={temp}
-                  weather={weather}
-                  cityName={cityName}
-                />
+					<>
+						{isLoading ? (
+							<Loader />
+						) : (
+							<>
+								<WeatherData
+									temp={temp}
+									weather={weather}
+									cityName={cityName}
+								/>
 
-                <div className={style.weatherBtn}>
-                  <ShowComponentButton
-                    text={
-                      showMode ? "Прогноз на 4 дня" : "Более детальный прогноз"
-                    }
-                    handleClick={handleShowModeComponent}
-                  />
-                </div>
+								<div className={style.weatherBtn}>
+									<ShowComponentButton
+										text={
+											showMode ? 'Прогноз на 4 дня' : 'Более детальный прогноз'
+										}
+										handleClick={handleShowModeComponent}
+									/>
+								</div>
 
-                {showMode ? (
-                  <WeatherSlider weatherDailyList={weatherDailyList} />
-                ) : (
-                  <WeatherSlider weatherDailyList={fiveDaysWeatherList} />
-                )}
+								{showMode ? (
+									<WeatherSlider weatherDailyList={weatherDailyList} />
+								) : (
+									<WeatherSlider weatherDailyList={fiveDaysWeatherList} />
+								)}
 
-                <DetailsWeatherData
-                  tempMin={tempMin}
-                  tempMax={tempMax}
-                  feelsLike={feelsLike}
-                  windSpeed={windSpeed}
-                  windDirection={windDirection}
-                  pressure={pressure}
-                  humidity={humidity}
-                />
-              </>
-            )}
-          </>
-        </div>
-      </Container>
-    </div>
-  );
+								<DetailsWeatherData
+									tempMin={tempMin}
+									tempMax={tempMax}
+									feelsLike={feelsLike}
+									windSpeed={windSpeed}
+									windDirection={windDirection}
+									pressure={pressure}
+									humidity={humidity}
+								/>
+							</>
+						)}
+					</>
+				</div>
+			</Container>
+		</div>
+	);
 };
 
 export default MainWeatherPageLayout;
